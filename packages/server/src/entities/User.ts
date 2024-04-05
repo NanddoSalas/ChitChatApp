5fe066d753be4d33cc606aac515a24bd310ac4e0
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-04-04 18:54:51 -0600

    update user role type

--- packages/server/src/entities/User.ts
+++ packages/server/src/entities/User.ts
@@ -7,7 +7,7 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { PublicUserData } from '../types';
+import { PublicUserData, UserRole } from '../types';
 import { DirectMessage } from './DirectMessage';
 import { Invitation } from './Invitation';
 import { Member } from './Member';
@@ -23,7 +23,7 @@ export class User extends BaseEntity {
   name: string;
 
   @Column({ default: 'Basic' })
-  role: string;
+  role: UserRole;
 
   @CreateDateColumn()
   createdAt: Date;
