ed4cf164c08b443cd61b0c195d9c1af58ce1b814
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-04-04 18:21:55 -0600

    update app middleware chain

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,7 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import 'reflect-metadata';
 
 export const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('Hello World');
