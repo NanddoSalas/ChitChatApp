58a84602e6bc3740caf65c4a4f399541f7f0d2ac
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-04-15 18:11:38 -0600

    add drawer and header components

---
+++ packages/web/src/componentes/Drawer.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+interface DrawerProps {
+  content: React.ReactNode;
+  side: React.ReactNode;
+}
+
+export const Drawer: React.FC<DrawerProps> = ({ content, side }) => {
+  return (
+    <div className="drawer">
+      <input id="sidebar-drawer" type="checkbox" className="drawer-toggle" />
+
+      <div className="drawer-content h-dvh">{content}</div>
+
+      <div className="drawer-side">
+        <label htmlFor="sidebar-drawer" className="drawer-overlay"></label>
+
+        {side}
+      </div>
+    </div>
+  );
+};
+
+export const OpenDraweButton = () => (
+  <label htmlFor="sidebar-drawer" className="btn btn-square btn-neutral">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  </label>
+);
---
+++ packages/web/src/componentes/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface HeaderProps {
+  start?: React.ReactNode;
+  center?: React.ReactNode;
+  end?: React.ReactNode;
+}
+
+export const Header: React.FC<HeaderProps> = ({ start, center, end }) => (
+  <div className="lg:hidden navbar bg-gray-900 p-4">
+    {start && <div className="navbar-start">{start}</div>}
+
+    {center && <div className="navbar-center">{center}</div>}
+
+    {end && <div className="navbar-end">{end}</div>}
+  </div>
+);
