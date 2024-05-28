469f977f9f8feaa14782a18fec57ff09c35a25a0
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-05-28 01:59:05 -0600

    init spring server package

---
+++ server/src/main/java/com/chitchatzone/server/ChitChatZoneServerApplication.java
@@ -0,0 +1,13 @@
+package com.chitchatzone.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitChatZoneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitChatZoneServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/chitchatzone/server/ChitChatZoneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitchatzone.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitChatZoneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
