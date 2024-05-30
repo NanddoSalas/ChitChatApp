d2f23a08d1baef9eddffa217a14bdfecdc804c3d
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-05-29 22:04:59 -0600

    add lombok maven dependency and refactor

--- server/src/main/java/com/chitchatzone/server/models/User.java
+++ server/src/main/java/com/chitchatzone/server/models/User.java
@@ -1,10 +1,8 @@
 package com.chitchatzone.server.models;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
-@Component
-@Scope("prototype")
+@Data
 public class User {
 
     private int id;
@@ -17,85 +15,5 @@ public class User {
     private String password;
     private String googleId;
     private String githubId;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(String creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getGoogleId() {
-        return googleId;
-    }
-
-    public void setGoogleId(String googleId) {
-        this.googleId = googleId;
-    }
-
-    public String getGithubId() {
-        return githubId;
-    }
-
-    public void setGithubId(String githubId) {
-        this.githubId = githubId;
-    }
 
 }
--- server/src/main/java/com/chitchatzone/server/repositories/UserRepository.java
+++ server/src/main/java/com/chitchatzone/server/repositories/UserRepository.java
@@ -1,32 +1,21 @@
 package com.chitchatzone.server.repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.chitchatzone.server.repositories.mappers.UserMapper;
 
+import lombok.Data;
+
 @Repository
+@Data
 public class UserRepository {
 
     JdbcTemplate template;
     UserMapper mapper;
 
-    public JdbcTemplate getTemplate() {
-        return template;
-    }
-
-    @Autowired
-    public void setTemplate(JdbcTemplate template) {
+    public UserRepository(JdbcTemplate template, UserMapper mapper) {
         this.template = template;
-    }
-
-    public UserMapper getMapper() {
-        return mapper;
-    }
-
-    @Autowired
-    public void setMapper(UserMapper mapper) {
         this.mapper = mapper;
     }
 
