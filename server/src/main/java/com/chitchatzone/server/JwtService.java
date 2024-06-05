c338cc82522b42fe6411c62e40e8ac9b400de1de
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-05 14:24:33 -0600

    add custom jwt authentication support

---
+++ server/src/main/java/com/chitchatzone/server/JwtService.java
@@ -0,0 +1,30 @@
+package com.chitchatzone.server;
+
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import com.chitchatzone.server.models.User;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class JwtService {
+
+    private final JwtEncoder jwtEncoder;
+
+    public Jwt generateAccessToken(User user) {
+
+        JwtClaimsSet claims = JwtClaimsSet.builder().subject(String.valueOf(user.getId()))
+                .claim("scope", user.getRole()).claim("email", user.getEmail()).build();
+
+        JwsHeader jwsHeader = JwsHeader.with(() -> "HS256").build();
+
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims));
+    }
+}
--- server/src/main/java/com/chitchatzone/server/security/WebSecurityConfig.java
+++ server/src/main/java/com/chitchatzone/server/security/WebSecurityConfig.java
@@ -1,17 +1,31 @@
 package com.chitchatzone.server.security;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +38,8 @@ public class WebSecurityConfig {
                 .sessionManagement(
                         (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/**")
-                        .permitAll().anyRequest().authenticated());
+                        .permitAll().anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
@@ -44,6 +59,26 @@ public class WebSecurityConfig {
         ProviderManager providerManager = new ProviderManager(authenticationProvider);
 
         return providerManager;
+    }
+
+    @Bean
+    public SecretKey generateSecretKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+
+        return keyGenerator.generateKey();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(SecretKey secretKey) {
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(SecretKey secretKey) {
+        JWKSource<SecurityContext> immutableSecret = new ImmutableSecret<>(secretKey);
+
+        return new NimbusJwtEncoder(immutableSecret);
     }
 
 }
