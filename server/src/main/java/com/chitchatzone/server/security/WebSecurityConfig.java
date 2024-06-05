7adc29f255d69d697f2a976dd179962ea48e3bbb
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-05 03:06:59 -0600

    update filter chain configuration
      * disable logout
      * remove basic authentication
      * disable cors

--- server/src/main/java/com/chitchatzone/server/security/WebSecurityConfig.java
+++ server/src/main/java/com/chitchatzone/server/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.chitchatzone.server.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,11 +15,12 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
+                .logout((logout) -> logout.disable())
                 .sessionManagement(
                         (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(withDefaults()).authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**").permitAll().anyRequest().authenticated());
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/**")
+                        .permitAll().anyRequest().authenticated());
 
         return http.build();
     }
