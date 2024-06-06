cb1b233018ca6c2291a2ab807b31af635dcec7d4
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-05 20:53:33 -0600

    refactor

--- server/src/main/java/com/chitchatzone/server/controllers/UserController.java
+++ server/src/main/java/com/chitchatzone/server/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chitchatzone.server.dtos.ResponseDTO;
@@ -12,13 +13,15 @@ import com.chitchatzone.server.services.UserService;
 
 import lombok.AllArgsConstructor;
 
+
 @RestController
+@RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping("/")
     public ResponseEntity<ResponseDTO> retrieveUsers() {
         List<UserDTO> userDTOs = userService.retrieveUsers();
 
