b40e1ecb3803ec2ab73ef46be6072254cfc7b4ba
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-15 15:56:02 -0600

    update Profile Button component

--- packages/web/src/componentes/Sidebar/components/ProfileButton.tsx
+++ packages/web/src/componentes/Sidebar/components/ProfileButton.tsx
@@ -1,21 +1,22 @@
+import { useLocation, useNavigate } from '@tanstack/react-router';
 import { useContext } from 'react';
 import { AuthContext } from '../../../AuthContext';
-import { useStore } from '../../../store';
 import { classNames } from '../../../utils';
 import { Avatar } from '../../Avatar';
 
 export const ProfileButton = () => {
-  const navigate = useStore((state) => state.navigate);
-  const current = useStore((state) => state.navigation.path) === '/account';
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const current = useLocation().pathname === '/account';
 
   const handleClick = () => {
-    navigate('/account', null);
+    if (!current) {
+      navigate({ to: '/account' });
+    }
   };
 
   return (
     <a
-      href="#"
       className={classNames(
         'flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800',
         current ? 'bg-gray-800' : '',
