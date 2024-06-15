852cbae15bece505fdcb07b276877354b0414352
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-15 11:28:17 -0600

    update App component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,27 @@
-import { MainScreen } from './Screens/MainScreen';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+import { AuthContextProvider } from './AuthContext';
 
 function App() {
-  return <MainScreen />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+          },
+        },
+      }),
+  );
+
+  return (
+    <AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
