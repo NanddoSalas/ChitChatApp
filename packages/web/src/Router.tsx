38598384f5a3730108321e68d9514f9726a3cd75
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-15 13:46:17 -0600

    update router tree

--- packages/web/src/Router.tsx
+++ packages/web/src/Router.tsx
@@ -2,9 +2,13 @@ import {
   Outlet,
   RouterProvider,
   createRootRoute,
+  createRoute,
   createRouter,
 } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { IndexScreen } from './screens/IndexScreen';
+import { SignInScreen } from './screens/SignInScreen';
+import { SignUpScreen } from './screens/SignUpScreen';
 
 const rootRoute = createRootRoute({
   component: () => (
@@ -15,7 +19,25 @@ const rootRoute = createRootRoute({
   ),
 });
 
-const routeTree = rootRoute.addChildren([]);
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: IndexScreen,
+});
+
+const signInRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/signin',
+  component: SignInScreen,
+});
+
+const signUpRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/signup',
+  component: SignUpScreen,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, signInRoute, signUpRoute]);
 
 const router = createRouter({
   routeTree,
