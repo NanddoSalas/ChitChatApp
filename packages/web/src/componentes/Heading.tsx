5f4243e9d648a2eeb56da6757b1d140311488815
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-16 17:23:44 -0600

    update Heading and Message Box components

--- packages/web/src/componentes/Heading.tsx
+++ packages/web/src/componentes/Heading.tsx
@@ -1,3 +1,5 @@
+import { classNames } from '../utils';
+
 interface HeadingProps {
   start?: React.ReactNode;
   center?: React.ReactNode;
@@ -5,11 +7,19 @@ interface HeadingProps {
 }
 
 export const Heading: React.FC<HeadingProps> = ({ start, center, end }) => (
-  <div className="navbar border-b border-gray-300 pb-5 flex items-center justify-between">
-    <div className="navbar-start">{start}</div>
+  <div
+    className={classNames(
+      'space-y-4 sm:space-y-6 lg:space-y-8 flex-1',
+      'pt-4 sm:pt-6 lg:pt-8',
+      'hidden lg:block',
+    )}
+  >
+    <div className="navbar border-b border-gray-300 pb-5 flex items-center justify-between">
+      <div className="navbar-start">{start}</div>
 
-    <div className="navbar-center">{center}</div>
+      <div className="navbar-center">{center}</div>
 
-    <div className="navbar-end">{end}</div>
+      <div className="navbar-end">{end}</div>
+    </div>
   </div>
 );
--- packages/web/src/componentes/MessageBox.tsx
+++ packages/web/src/componentes/MessageBox.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
+import { classNames } from '../utils';
 
 interface MessageBoxProps {
-  target: 'user' | 'room';
-  targetId: number;
+  onSendMessage: (body: string) => void;
 }
 
-export const MessageBox: React.FC<MessageBoxProps> = () => {
+export const MessageBox: React.FC<MessageBoxProps> = ({ onSendMessage }) => {
   const [value, setValue] = useState('');
 
   const handleSendMessage = () => {
     if (value) {
+      onSendMessage(value);
       setValue('');
     }
   };
@@ -27,30 +28,39 @@ export const MessageBox: React.FC<MessageBoxProps> = () => {
   };
 
   return (
-    <div className="flex justify-end items-center">
-      <textarea
-        className="textarea textarea-bordered w-full resize-none pr-[72px] overflow-hidden"
-        placeholder="Type your message..."
-        rows={2}
-        value={value}
-        onChange={handleChange}
-      />
+    <div
+      className={classNames(
+        'px-4 sm:px-6 lg:px-8 space-y-4 sm:space-y-6 lg:space-y-8 flex-1',
+        'pb-4 sm:pb-6 lg:pb-8',
+        'pt-4 sm:pt-6 lg:pt-8',
+        'border-t border-gray-300',
+      )}
+    >
+      <div className="flex justify-end items-center">
+        <textarea
+          className="textarea textarea-bordered w-full resize-none pr-[72px] overflow-hidden"
+          placeholder="Type your message..."
+          rows={2}
+          value={value}
+          onChange={handleChange}
+        />
 
-      <div className="absolute mr-[12px]">
-        <button
-          className="btn btn-ghost btn-square"
-          disabled={!value}
-          onClick={handleSendMessage}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
+        <div className="absolute mr-[12px]">
+          <button
+            className="btn btn-ghost btn-square"
+            disabled={!value}
+            onClick={handleSendMessage}
           >
-            <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
