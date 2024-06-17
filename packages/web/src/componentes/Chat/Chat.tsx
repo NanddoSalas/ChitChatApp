a1155c31e7040018d7f10192268a6fee19ef1366
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-16 19:11:22 -0600

    remove unused files

--- packages/web/src/componentes/Chat/Chat.tsx
+++
@@ -1,7 +0,0 @@
-export const Chat = () => {
-  // const navigation = useStore((state) => state.navigation);
-  // const roomMessages = useStore((state) => state.roomMessages);
-  // const directMessages = useStore((state) => state.directMessages);
-
-  return <div></div>;
-};
