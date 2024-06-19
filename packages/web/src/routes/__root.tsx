78f0081571b11d75e3f64f4fba24d82ee3896b5a
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-18 20:55:05 -0600

    update root route and Index Screen

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -1,22 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { Outlet, createRootRoute, useNavigate } from '@tanstack/react-router';
-import { useContext, useEffect } from 'react';
-import { AuthContext } from '../AuthContext';
-import { DashboardScreen } from '../screens/DashboardScreen';
+import { createRootRoute } from '@tanstack/react-router';
+import { IndexScreen } from '../screens/IndexScreen';
 
 export const Route = createRootRoute({
-  component: () => {
-    const navigate = useNavigate();
-    const { isAuthenticated } = useContext(AuthContext);
-
-    useEffect(() => {
-      if (!isAuthenticated) {
-        navigate({ to: '/welcome' });
-      }
-    }, [isAuthenticated, navigate]);
-
-    if (!isAuthenticated) return <Outlet />;
-
-    return <DashboardScreen />;
-  },
+  component: IndexScreen,
 });
--- packages/web/src/screens/IndexScreen.tsx
+++ packages/web/src/screens/IndexScreen.tsx
@@ -1,52 +1,25 @@
 import { useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
+import { Outlet, useNavigate } from '@tanstack/react-router';
 import { useContext, useEffect } from 'react';
 import { AuthContext } from '../AuthContext';
 import { DashboardScreen } from './DashboardScreen';
-import { WelcomeScreen } from './WelcomeScreen';
 
 export const IndexScreen = () => {
-  const { isAuthenticated, accessToken } = useContext(AuthContext);
+  const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
 
   useEffect(() => {
-    queryClient.setDefaultOptions({
-      queries: {
-        queryFn: async ({ queryKey }) => {
-          try {
-            const res = await axios.get('http://localhost:8080' + queryKey, {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-              },
-            });
-
-            if (res.status === 200) {
-              return res.data;
-            }
-
-            console.log(res);
-
-            return { data: null, errors: null };
-          } catch (error) {
-            console.log(error);
-
-            return { data: null, errors: null };
-          }
-        },
-      },
-    });
-  }, [accessToken, queryClient]);
-
-  useEffect(() => {
     if (!isAuthenticated) {
+      navigate({ to: '/welcome' });
       queryClient.clear();
       queryClient.getQueryCache().clear();
     }
-  }, [isAuthenticated, queryClient]);
+  }, [isAuthenticated, navigate, queryClient]);
 
   if (isAuthenticated) {
     return <DashboardScreen />;
   }
 
-  return <WelcomeScreen />;
+  return <Outlet />;
 };
