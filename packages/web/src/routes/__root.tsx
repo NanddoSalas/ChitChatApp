9327e0e9686e5b4259d02fc1b05781b424b4c2d3
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-22 19:45:37 -0600

    hook up tanstack router devtools

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -1,7 +1,24 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { createRootRoute } from '@tanstack/react-router';
+import React from 'react';
 import { IndexScreen } from '../screens/IndexScreen';
 
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
+
 export const Route = createRootRoute({
-  component: IndexScreen,
+  component: () => (
+    <>
+      <IndexScreen />
+      <TanStackRouterDevtools />
+    </>
+  ),
 });
