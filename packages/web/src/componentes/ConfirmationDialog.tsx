c387b53b9a2db027506d21af45377a8b5673154b
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-06-24 18:41:08 -0600

    refactor

--- packages/web/src/componentes/ConfirmationDialog.tsx
+++ packages/web/src/componentes/ConfirmationDialog.tsx
@@ -1,107 +1,65 @@
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog } from '@headlessui/react';
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
-import { Fragment, useRef } from 'react';
 import { useStore } from '../store';
 import { classNames } from '../utils';
+import { Modal } from './Modal';
 
 export const ConfirmationDialog: React.FC = () => {
   const closeDialog = useStore((state) => state.closeConfirmationDialog);
   const { dialog, isOpen } = useStore((state) => state.confirmationDialog);
 
-  const cancelButtonRef = useRef(null);
-
   return (
-    <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={() => closeDialog(false)}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+    <Modal isOpen={isOpen} onClose={() => closeDialog(false)}>
+      <div className="sm:flex sm:items-start">
+        <div
+          className={classNames(
+            'mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10',
+            dialog?.status === 'danger' ? 'bg-red-100' : 'bg-blue-100',
+          )}
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+          <ExclamationTriangleIcon
+            className={classNames(
+              'h-6 w-6',
+              dialog?.status === 'danger' ? 'text-red-600' : 'text-blue-600',
+            )}
+            aria-hidden="true"
+          />
+        </div>
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                <div className="sm:flex sm:items-start">
-                  <div
-                    className={classNames(
-                      'mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10',
-                      dialog?.status === 'danger'
-                        ? 'bg-red-100'
-                        : 'bg-blue-100',
-                    )}
-                  >
-                    <ExclamationTriangleIcon
-                      className={classNames(
-                        'h-6 w-6',
-                        dialog?.status === 'danger'
-                          ? 'text-red-600'
-                          : 'text-blue-600',
-                      )}
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-base font-semibold leading-6 text-gray-900"
-                    >
-                      {dialog?.title}
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">{dialog?.text}</p>
-                    </div>
-                  </div>
-                </div>
+        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+          <Dialog.Title
+            as="h3"
+            className="text-base font-semibold leading-6 text-gray-900"
+          >
+            {dialog?.title}
+          </Dialog.Title>
 
-                <div className="mt-5 sm:mt-4 flex justify-end space-x-4">
-                  <button
-                    type="button"
-                    className={classNames(
-                      'btn btn-neutral btn-outline btn-sm lg:btn-md',
-                    )}
-                    onClick={() => closeDialog(false)}
-                    ref={cancelButtonRef}
-                  >
-                    Cancel
-                  </button>
-
-                  <button
-                    type="button"
-                    className={classNames(
-                      'btn btn-neutral btn-sm lg:btn-md',
-                      dialog?.status === 'danger' ? 'btn-error' : 'btn-info',
-                    )}
-                    onClick={() => closeDialog(true)}
-                  >
-                    {dialog?.actionPlaceholder}
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
+          <div className="mt-2">
+            <p className="text-sm text-gray-500">{dialog?.text}</p>
           </div>
         </div>
-      </Dialog>
-    </Transition.Root>
+      </div>
+
+      <div className="mt-5 sm:mt-4 flex justify-end space-x-4">
+        <button
+          type="button"
+          className={classNames('btn btn-neutral btn-outline btn-sm lg:btn-md')}
+          onClick={() => closeDialog(false)}
+        >
+          Cancel
+        </button>
+
+        <button
+          type="button"
+          className={classNames(
+            'btn btn-neutral btn-sm lg:btn-md',
+            dialog?.status === 'danger' ? 'btn-error' : 'btn-info',
+          )}
+          onClick={() => closeDialog(true)}
+        >
+          {dialog?.actionPlaceholder}
+        </button>
+      </div>
+    </Modal>
   );
 };
--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -8,8 +8,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [forms, daisyui, typography],
+  plugins: [forms, typography, daisyui],
   daisyui: {
-    themes: ["light"],
+    themes: ['light'],
   },
 };
