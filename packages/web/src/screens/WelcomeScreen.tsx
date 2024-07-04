111503c90ba021d980dc8bb1f37e0301b6570571
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-07-03 19:27:17 -0600

    implement Welcome Screen

--- packages/web/src/screens/WelcomeScreen.tsx
+++ packages/web/src/screens/WelcomeScreen.tsx
@@ -1,12 +1,47 @@
 import { Link } from '@tanstack/react-router';
+import { GitHubButton } from '../componentes/Buttons/GitHubButton';
+import { Logo } from '../componentes/Logo';
 
 export const WelcomeScreen = () => {
   return (
-    <div>
-      <div>Welcome</div>
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-50">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <div className="space-y-6">
+            <Logo />
 
-      <Link to="/auth/signin">SignIn</Link>
-      <Link to="/auth/signup">SignUp</Link>
+            <Link
+              className="btn btn-neutral btn-outline w-full"
+              to="/auth/signin"
+            >
+              Sign in
+            </Link>
+
+            <Link
+              className="btn btn-neutral btn-outline w-full"
+              to="/auth/signup"
+            >
+              Sign up
+            </Link>
+
+            <GitHubButton
+              placeholder="Source Code"
+              onClick={() => window.open('https://github.com/NanddoSalas')}
+            />
+          </div>
+        </div>
+      </div>
+
+      <p className="mt-10 text-center text-sm ">
+        Build by{' '}
+        <a
+          href="https://github.com/NanddoSalas"
+          target="_blank"
+          className="text-blue-600"
+        >
+          NanddoSalas
+        </a>
+      </p>
     </div>
   );
 };
