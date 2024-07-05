b4a9b8e5b9f6146e99bb2856b22654c21e6c93d3
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-07-04 20:04:42 -0600

    update Home Screen

--- packages/web/src/screens/SubScreens/HomeScreen.tsx
+++ packages/web/src/screens/SubScreens/HomeScreen.tsx
@@ -1,9 +1,32 @@
+import { useContext } from 'react';
+import Avatar, { AvatarFullConfig, genConfig } from 'react-nice-avatar';
+import { AuthContext } from '../../AuthContext';
 import { OpenDraweButton } from '../../componentes/Drawer';
 import { Header } from '../../componentes/Header';
 import { SubScreenContainer } from '../../componentes/SubScreenContainer';
-import { SubScreenLayout } from '../../componentes/SubScreenLayout';
 
 const HomeScreen = () => {
+  const { user } = useContext(AuthContext);
+
+  const config: AvatarFullConfig = {
+    sex: 'man',
+    faceColor: '#F9C9B6',
+    earSize: 'small',
+    eyeStyle: 'circle',
+    noseStyle: 'long',
+    mouthStyle: 'laugh',
+    shirtStyle: 'polo',
+    glassesStyle: 'round',
+    hairStyle: 'thick',
+    hairColor: '#F9C9B6',
+    hatStyle: 'none',
+    eyeBrowStyle: 'up',
+    shirtColor: '#2b3440',
+    bgColor: '#e5e7eb',
+  };
+
+  const myConfig = genConfig(config);
+
   return (
     <SubScreenContainer>
       <Header
@@ -12,7 +35,27 @@ const HomeScreen = () => {
         end={<></>}
       />
 
-      <SubScreenLayout>{null}</SubScreenLayout>
+      <div className="flex h-2/3 justify-center items-center">
+        <div className="chat chat-start flex items-center opacity-50 m-6">
+          <div className="chat-image avatar">
+            <Avatar className="w-24 h-24" {...myConfig} />
+          </div>
+
+          <div className="chat-bubble prose">
+            <h3 className="text-gray-100">
+              Hi {user?.fullName}, How is it going?
+            </h3>
+
+            <h3 className="text-gray-100 hidden lg:block">
+              Type 'ctr + k' to open the navigation palette!
+            </h3>
+
+            <h3 className="text-gray-100 lg:hidden">
+              Press the hamburger button to start navigating!
+            </h3>
+          </div>
+        </div>
+      </div>
     </SubScreenContainer>
   );
 };
