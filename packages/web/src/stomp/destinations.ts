970dc8e5674c5f1f83b02b175215a277e9a488b2
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2024-07-07 19:46:09 -0600

    design architecture of stomp's subscriptions

---
+++ packages/web/src/stomp/destinations.ts
@@ -0,0 +1,22 @@
+export const NewUserDestination = '/topic/new-user';
+
+export const UpdateUserDestination = '/topic/update-user';
+
+export const NewRoomDestination = '/topic/new-room';
+
+export const UpdateRoomDestination = '/topic/update-room';
+
+export const DeleteRoomDestination = '/topic/delete-room';
+
+export const NewRoomMessageDestination = '/topic/new-room-message';
+
+export const UpdateRoleDestination = '/user/queue/update-role';
+
+export const NewPrivateRoomMessageDestination =
+  '/user/queue/new-private-room-message';
+
+export const NewDirectMessageDestination = '/user/queue/new-direct-message';
+
+export const NewRoomMemberDestination = '/user/queue/new-room-member';
+
+export const DeleteRoomMemberDestination = '/user/queue/delete-room-member';
